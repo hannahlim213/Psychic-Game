63cef30af4e7dc42d4da75d8ef4e853acb605328
Han Ban <hannah15198@gmail.com>
2018-04-27 15:46:41 -0700

    Final one with notes included.

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# Word-Guess-Game
+# Psychic-Game
+
+One Paragraph of project description goes here
+
+## Getting Started
+
+This game is about guessing what letter the computer is thinking of. Press a key from a-z and the score board will update accordingly. Lets see if you can read the computer's mind!
+
+## Built With
+
+* HTML
+* CSS
+* Javascript
+* Visual Studio
+
+## Authors
+
+* **Hannah Lim** - *Initial work* - [hannahlim213](https://github.com/hannahlim213)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,44 +1,48 @@
 // DECLARE VARIABLES
 // Declare letters, starting Wins, Losses and Guesses left.
 
-var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","x","y","z"];
-var winsDisplay = document.getElementById("wins");  //same as $("#wins")
-var lossesDisplay = document.getElementById("losses");
-var guessDisplay = document.getElementById("guessLeft");
-var userGuessDisplay = document.getElementById("userGuess");
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"];
+var winsDisplay = 0; //document.getElementById("wins").textContent;  //same as $("#wins")
+var lossesDisplay = 0; //document.getElementById("losses").textContent;
+var guessDisplay = 10; //document.getElementById("guessLeft").textContent;
+var userGuessDisplay = []; //document.getElementById("userGuess").textContent;
 
-
-
-var userGuess;
-
+// Declare letters user has clicked and the letter computer has chosen
 var computerGuess = letters[Math.floor(Math.random() * letters.length)];
-console.log (computerGuess)
+console.log(computerGuess)
 
 
 // FUNCTIONS
-function updateScore() {
-    if (userGuessDisplay === computerGuess) {
-        winsDisplay = winsDisplay++;
-        winsDisplay = document.getElementById("wins").textContent;
+function reset(){
+    winsDisplay = 0;
+    lossesDisplay = 0;
+    guessDisplay = 0;
+    userGuessDisplay = [];
+}
+function updateScore(chicken) {
+    if (chicken === computerGuess) {
+        winsDisplay++;
+        document.getElementById("wins").innerHTML = winsDisplay;
+
         console.log("Correct!")
     }
     else {
-        lossesDisplay = lossesDisplay++;
-        lossesDisplay = document.getElementById("losses").textContent;
+        console.log("ug", chicken)
+        console.log("cg", computerGuess)
+        lossesDisplay++;
+        document.getElementById("losses").innerHTML = lossesDisplay;
         console.log("Wrong!")
     }
 
 }
 
-function updateUserGuessDisplay() {
-    for (var i=0; i<letters.length; i++) {
-        userGuessDisplay = userGuessDisplay[i];
-        userGuessDisplay = document.getElementById("userGuess").textContent;
-        guessDisplay = GuessDisplay--;
-        guessDisplay = document.getElementById("guessLeft").textContent;
-        console.log(userGuessDisplay + guessDisplay)
-    }
-}
+// function updateUserGuessDisplay() {
+//     for (var i=0; i<letters.length; i++) {
+//         userGuessDisplay = letters[i];
+//         document.getElementById("userGuess").innerHTML = guessDisplay;
+//         //console.log(userGuessDisplay + guessDisplay)
+//     }
+// }
 
 // function updateGuessDisplay() {
 //     guessDisplay = GuessDisplay--;
@@ -49,25 +53,55 @@ function updateUserGuessDisplay() {
 
 // MAIN PROCESS
 
-
 // When user presses a key, go through this process
-document.onkeyup = function(event) {
-    
-    // If the guessDisplay is more than 0, run functions
-    if (guessDisplay = 0) {
-        return;
-    } 
-    
-    // Determine which key was pressed, make it lowercase and set it to userGuessDisplay
-    userGuess = event.key.toLowerCase();
+document.onkeyup = function (event) {
 
-    // Only run if computerGuess equals userGuess
-    if (userGuess === computerGuess) {
-        updateScore();
-        updateUserGuessDisplay();
-        // updateGuessDisplay();
+    // If the guessDisplay is more than 0, run functions
+    // if (guessDisplay<= 0) {
+    //     return;
+    // } 
+
+
+    // Determine which key was pressed, make it lowercase and set it to userGuessDisplay
+    var userGuess = event.key.toLowerCase();
+    // check if value is in arrary
+
+    for (var i = 0; i < userGuessDisplay.length; i++) {
+        console.log("hellur")
+        if (userGuess === userGuessDisplay[i]) {
+            alert("You already guessed that letter.")
+            return;
+        }
+        
     }
-   
-    
-}
+
+
+
+
+
+        userGuessDisplay.push(userGuess)
+        console.log(userGuessDisplay)
+        // Only run if computerGuess equals userGuess
+        if (userGuess === computerGuess) {
+            updateScore(userGuess);
+
+            // updateGuessDisplay();
+        }
+        else {
+            //updateUserGuessDisplay();
+            updateScore(userGuess);
+            guessDisplay--;
+            document.getElementById("userGuess").innerHTML = userGuessDisplay;
+
+        }
+       
+
+    }
+
+
+
+
+
+
+
 
